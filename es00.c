6dab38f2116eb9f10e35b7e3c5dda3f904937334
MarcusHollowaywd2 <128369773+MarcusHollowaywd2@users.noreply.github.com>
2023-03-20 12:35:53 +0100

    Add files via upload

---
+++ es00.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+float x = 4.7;
+char e =  'E';
+char l = 'L';
+int intero = 72;
+
+printf("ho creato una variabile float %f, il primo carattere è %c, il secondo carattere è %c, il terzo carattere è con variabile int %c\n", 4.7, e, l, intero);
+}
+ 
